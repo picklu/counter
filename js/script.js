5ff3e479bece56b2b25f2ef02b0804af63ec64f9
Subrata Sarker <subrata_sarker@yahoo.com>
2018-11-10 15:39:59 +0900

    Add script for spelling the cardinals

--- js/script.js
+++ js/script.js
@@ -4,6 +4,65 @@ const btnReset = document.getElementById("reset");
 const inputValue = document.getElementById("value");
 const minValue = 0;
 const maxValue = 1000;
+const cardinals = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "10": "ten",
+    "11": "eleven",
+    "12": "twelve",
+    "13": "thirteen",
+    "14": "fourteen",
+    "15": "fifteen",
+    "16": "sixteen",
+    "17": "seventeen",
+    "18": "eighteen",
+    "19": "nineteen",
+    "20": "twenty",
+    "30": "thirty",
+    "40": "forty",
+    "50": "fifty",
+    "60": "sixty",
+    "70": "seventy",
+    "80": "eighty",
+    "90": "ninety",
+    "100": "one hundred",
+    "200": "two hundred",
+    "300": "three hundred",
+    "400": "four hundred",
+    "500": "five hundred",
+    "600": "six hundred",
+    "700": "seven hundred",
+    "800": "eight hundred",
+    "900": "nine hundred",
+    "1000": "one thousand"
+};
+const cardinalsArr = Object.keys(cardinals).reverse();
+
+const spellTheNumber = number => {
+    let spelledNumberArr = [];
+    for (let i = 0; i < cardinalsArr.length; i++) {
+        let eachNumber = cardinalsArr[i];
+        let cardinalNumber = parseInt(eachNumber);
+        let remainder = number % cardinalNumber;
+        
+        if (number == cardinalNumber) {
+            spelledNumberArr.push(cardinals[eachNumber]);
+            break;
+        }
+        else if (number > cardinalNumber) {
+            spelledNumberArr.push(cardinals[eachNumber]);
+            number = remainder;             
+        }
+    }
+    return spelledNumberArr.join(" ").toUpperCase();
+};
 
 const updateValue = how => {
     let value = Number(inputValue.value);
